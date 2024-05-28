8666ed87f4c054578b31346ee44f7ed922f87379
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-05-28 23:14:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://forward-alexis-dplove.koyeb.app/")
+URL = environ.get("URL", "https://mraktest001-52c115ffc630.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
